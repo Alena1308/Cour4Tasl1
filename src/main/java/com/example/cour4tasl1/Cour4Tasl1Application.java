33b24071bef5c8ac3a7e42f3b59c735df37b7765
Alena1308 <Lenamironova13@mail.ru>
2023-02-27 12:31:47 +0300

    Initial commit

---
+++ src/main/java/com/example/cour4tasl1/Cour4Tasl1Application.java
@@ -0,0 +1,13 @@
+package com.example.cour4tasl1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cour4Tasl1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cour4Tasl1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cour4tasl1/Cour4Tasl1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cour4tasl1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cour4Tasl1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
